fdffbb5333317639f9af39cab2172b858ce94eb9
akeiosan <69976639+akeiosan@users.noreply.github.com>
2020-12-01 19:29:37 +0300

    Add files via upload

---
+++ Xödevi.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+main()
+{
+	for(int i=1;i<=7;i++)
+	{
+		
+		for(int j=1;j<=7;j++)
+		
+		{
+			
+			if(i+j==8 || i==j)
+			printf("%c",'X');
+			
+			else
+			printf("%c",' ');
+			
+			
+			
+			
+			
+		}
+		
+		
+		printf("\n");
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
